68ad0d90f0ea961d4e0285e38b2a494d637dbe54
kevinkarlsson94 <kiwii.94@hotmail.com>
2016-06-13 01:25:08 +0900

    Contact form + smooth scroll + bg-update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-$(document).ready(function() {
-    alert("hi") 
-};
+$(function() {
+  setBindings();
+});
 
 function setBindings(){
     $("nav a").click(function(e){
+        e.oreventDefault;
+        var sectionID = e.currentTarget.id + "Section";
        
+        $("html body").animate({
+           scrollTop : $("#" + sectionID).offset().top-250
+        },1000);
+        
     });
 }
